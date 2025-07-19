20fcdfa46cc38839c9bdd33bf0c7fb8802ed4a9d
Folombas <folombas@yandex.ru>
2025-07-19 16:53:57 +0300

    imported external color package

---
+++ helloGo/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/fatih/color"
+)
+
+func main() {
+	fmt.Println("Hello, Go Programming Language!")
+	color.Red("This is red text")
+	color.Green("This is green text")
+}
---
+++ variablesGo/main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	var info string = "Green Tea price is 50₽"
+
+	fmt.Println(info)
+}
