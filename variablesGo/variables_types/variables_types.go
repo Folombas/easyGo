9d61ce85eb303adf70727291008d585ac60499db
Folombas <folombas@yandex.ru>
2025-07-20 22:38:30 +0300

    Определение типа переменных в Go

---
+++ variablesGo/variables_types/variables_types.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	name := "Green Tea Flying Dragon"
+	price := 2.55 // default is float64
+	ready := true
+	shopCount := 8
+	var stockCount int64 = 2000
+
+	fmt.Printf("Type of name is: %T\n", name)
+	fmt.Printf("Type of price is: %T\n", price)
+	fmt.Printf("Type of ready is: %T\n", ready)
+	fmt.Printf("Type of shopCount is: %T\n", shopCount)
+	fmt.Printf("Type of stockCount is: %T\n", stockCount)
+}
