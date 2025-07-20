99cfab4a7ecda9600926a067c9e6b223c28f9011
Folombas <folombas@yandex.ru>
2025-07-20 22:04:49 +0300

    just some commit

--- variablesGo/basic_declaration/basic_declaration.go
+++ variablesGo/basic_declaration/basic_declaration.go
@@ -1,10 +1,8 @@
 package main
 
-import "fmt"
-
 func main() {
-	name 	:= "Green Tea Flying Dragon"
-	price 	:=  1.22
-	ready 	:= true
-	count 	:= 6
+	name := "Green Tea Flying Dragon"
+	price := 1.22
+	ready := true
+	count := 6
 }
