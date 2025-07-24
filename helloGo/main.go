b6f028f2aa9ed7ad5183a7807f9c11346e693bb0
Folombas <folombas@yandex.ru>
2025-07-24 22:26:24 +0300

    I continue to study the Go programming language!

--- helloGo/main.go
+++ helloGo/main.go
@@ -10,4 +10,5 @@ func main() {
 	fmt.Println("Hello, Go Programming Language!")
 	color.Red("This is red text")
 	color.Green("This is green text")
+	color.Magenta("I continue to study the Go programming language!")
 }
