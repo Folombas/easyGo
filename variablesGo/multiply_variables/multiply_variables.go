749bda2dbf76940c56fde19154080884ed0e325b
Folombas <personalmessagesforfolomkers@gmail.com>
2026-04-19 22:29:27 +0300

    ☕ кофейня: теперь можно выбрать размер стакана (маленький, средний, большой)

--- variablesGo/multiply_variables/multiply_variables.go
+++ variablesGo/multiply_variables/multiply_variables.go
@@ -3,17 +3,51 @@ package main
 import "fmt"
 
 func main() {
-	// Явное объявление переменных
-	var teaName string = "Black Tea"
-	var hotCoffee string = "Hot coffee"
+	// Цены на чай и кофе в зависимости от размера
+	teaPrices := map[string]float64{
+		"маленький": 50,
+		"средний":   70,
+		"большой":   90,
+	}
+	coffeePrices := map[string]float64{
+		"маленький": 80,
+		"средний":   100,
+		"большой":   120,
+	}
 
-	// Неявное объявление переменных
-	var size = "Little"
-	var sizeCoffee = "Middle"
+	// Доступные размеры
+	sizes := []string{"маленький", "средний", "большой"}
 
-	var price = 50.00
-	var priceCoffee = 100.00
+	fmt.Println("Добро пожаловать в кофейню!")
+	fmt.Println("Выберите размер стакана для чая:")
+	for i, size := range sizes {
+		fmt.Printf("%d. %s\n", i+1, size)
+	}
+	var teaChoice int
+	fmt.Scan(&teaChoice)
+	if teaChoice < 1 || teaChoice > 3 {
+		fmt.Println("Некорректный выбор, установлен 'маленький'")
+		teaChoice = 1
+	}
+	teaSize := sizes[teaChoice-1]
+	teaPrice := teaPrices[teaSize]
 
-	fmt.Println("Little Black Tea price is ₽50.00")
-	fmt.Println(size, teaName, "price is ₽", price)
+	fmt.Println("Выберите размер стакана для кофе:")
+	for i, size := range sizes {
+		fmt.Printf("%d. %s\n", i+1, size)
+	}
+	var coffeeChoice int
+	fmt.Scan(&coffeeChoice)
+	if coffeeChoice < 1 || coffeeChoice > 3 {
+		fmt.Println("Некорректный выбор, установлен 'маленький'")
+		coffeeChoice = 1
+	}
+	coffeeSize := sizes[coffeeChoice-1]
+	coffeePrice := coffeePrices[coffeeSize]
+
+	// Вывод информации
+	fmt.Println("\n--- Ваш заказ ---")
+	fmt.Printf("%s чай стоит: %.2f руб.\n", teaSize, teaPrice)
+	fmt.Printf("%s кофе стоит: %.2f руб.\n", coffeeSize, coffeePrice)
+	fmt.Printf("Итого: %.2f руб.\n", teaPrice+coffeePrice)
 }
