b1980187959b3cf61c7564ead478952feb40e603
Folombas <personalmessagesforfolomkers@gmail.com>
2026-04-20 22:18:56 +0300

    Added file demo.go in module gocommand

---
+++ gocommand/demo.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+// DemoFunction показывает пример полезной функции
+func DemoFunction() {
+	fmt.Println("\n✨ Пример работы функции из demo.go:")
+	fmt.Println("   Вызов DemoFunction() успешно выполнен.")
+}
